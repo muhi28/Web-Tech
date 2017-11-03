8d2c839b35916b91b213fc83335e9c1894596fc0
thompson <thauer@edu.aau.at>
2017-11-03 11:08:01 +0100

    > 3.2. fin >> Better .php

--- UE3/3.2/windowCreator.js
+++ UE3/3.2/windowCreator.js
@@ -1,8 +1,10 @@
 /** Sources:
 /*  https://wiki.selfhtml.org/wiki/JavaScript/Window/open
+ * https://www.w3schools.com/jsref/met_document_createelement.asp
  */
 
 function popup() {
+    // Define new Window
     var url = document.getElementById("url").value;
     var title = document.getElementById("title").value;
     var x = document.getElementById("x").value;
@@ -14,19 +16,30 @@ function popup() {
         ",scrollbars=0" +   // ??? Assuming lack of scrollbar with content
         ",status=0" +       // ??? New Window
         ",titlebar=0" +     // New window. Why even
-        ",toolbar=0";
+        ",toolbar=0";       // What the fuck
     var replace = false;
 
+    // Create Window
     var w = window.open("", title, windowFeatures, replace);
 
-    <!--Placing Windows is deemed a """Security Issue in Chrome""", thus: -->
-    /**
-    /* Not: document.documentElement.offsetWidth
-     */
+    // Move Window
     var left = (window.innerWidth/2);   // Middle of current window
     var top = (window.innerHeight/2);   // Middle of current window
     w.moveTo(left, top);
 
     // For Debugging:
-    //w.document.write("Hello world. My x is " + x + "My y is " + y + "My screenX is: " +left + "myScreenY is : "+ top);
+    //w.document.write("Hello world. <br>My x is " + x + "<br>My y is " + y + "<br>My screenX is: " +left + "<br>myScreenY is : "+ top + "<br><br>");
+
+    // Create Button
+    var button = document.createElement("button");
+    button.innerHTML = "Close";
+
+    // Append to body
+    var body = w.document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+
+    // Add Listener
+    button.addEventListener ("click", function() {
+        w.close();
+    });
 }

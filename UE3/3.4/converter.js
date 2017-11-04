52a9af89652a36abf656a0d2480d471a82b49199
thompson <thauer@edu.aau.at>
2017-11-04 19:34:33 +0100

    > 3.4 Fin >> WHY GOD

--- UE3/3.4/converter.js
+++ UE3/3.4/converter.js
@@ -1,46 +1,34 @@
 function calculate() {
+    // Setup
+    var val;
     // Fetch
+    val = document.getElementById("uscur").value;
 
-    var aCur = document.getElementById("acur").value;
-    var usCur = document.getElementById("uscur").value;
+    console.log("Val: " + val + ":: Type of: " + typeof val);
 
     // Check String for faults :: https://stackoverflow.com/questions/6603015/check-whether-a-string-matches-a-regex
     var re = new RegExp("(\\*|\\/)+[0-9]+"); // + and - reads alright
-    if(re.test(usCur) || re.test(aCur)){
-        console.log("Dirty value was read. Result may vary.")
+    if(re.test(val)){
+        console.log("A dirty value was read. Result may vary.")
+    }else{
+        console.log("Value is fine.")
     }
+    // TODO: Add regex which controls ',' locations ... 302,312,312 & not: 30,23123,12
 
     // Search String for multiple ',' and change
     /** In .js, Strings are immutable :: https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript **/
-    var index;
-    while((index = usCur.search(",")) !== -1){
-        transform(usCur, index);
-        console.log(usCur);
+    var index = val.search(",");
+    var pre, suf;
+    while(index >= 0){
+        pre = val.substr(0,index);              // Exklusive index
+        suf = val.substr(index+1, val.length);  // Inklusive index => +1
+
+        val = pre+'.'+suf;
+        index = val.search(",");
     }
+    // [123,123,123] => [123.123,123] => [123.123.123]
+    // Val is now in european format
 
-
-    // FIXME below
-
-
-    // Parse data
-    var usVal = parseFloat(usCur);
-    var aVal = parseFloat(aCur);
-
-
-    console.log("usVar: " + usCur + " :: Type of: " + typeof usCur + "\naVar: " + aCur + " :: Type of: " + typeof aCur);
-    console.log("usVal: " + usVal + " :: Type of: " + typeof usVal + "\naVal: " + aVal + " :: Type of: " + typeof aVal);
-
-    if (usVal == NaN && aVal == NaN){
-        console.log("\n\tEnter a value");
-    }
-    if(usVal == NaN){
-        calc_AustriaToAmerica();
-    }
-    if(aVal == NaN){
-        calc_AmericaToAustria();
-    }
-}
-
-function transform(string, index){
-    return string.substr(0, index) + "," + (index, string.length);
+    // Pass Data into Element
+    document.getElementById("acur").value = val;
 }
